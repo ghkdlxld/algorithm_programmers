e4ea101afa2f92343f88cb477d4822360fb6b9b7
ghkdlxld <ohsg97@gmail.com>
2022-01-09 01:06:38 +0900

    숫자 문자열과 영단어

--- 숫자_문자열과_영단어.py
+++ 숫자_문자열과_영단어.py
@@ -1,3 +1,12 @@
 def solution(s):
-    answer = 0
-    return answer
+    lst = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+    answer = ''
+    for i in range(len(s)):
+        if s[i].isdigit():
+            answer += s[i]
+        else:
+            for j in lst:
+                if s[i:i+len(j)] == j:
+                    answer += str(lst.index(j))
+                    break
+    return int(answer)
