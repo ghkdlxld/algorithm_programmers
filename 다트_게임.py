9bdcf84b385cf4810af39b9d868602e6bdac9ada
ghkdlxld <ohsg97@gmail.com>
2022-01-24 23:04:45 +0900

    다트 게임

--- 다트_게임.py
+++ 다트_게임.py
@@ -1,4 +1,11 @@
 def solution(dartResult):
-    print(dartResult)
-    answer = 0
-    return answer
+    arr = []
+    i = 0
+    while i < len(dartResult):
+        if dartResult[i].isdigit() and dartResult[i+1].isdigit():
+            arr.append(10)
+            i += 2
+        else:
+            arr.append(dartResult[i])
+            i += 1
+    return arr
