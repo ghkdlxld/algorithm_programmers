25268430a5af3b4b5412458c93f4248b5bc73511
ghkdlxld <ohsg97@gmail.com>
2022-01-25 00:37:12 +0900

    다트 게임

--- 다트_게임.py
+++ 다트_게임.py
@@ -1,11 +1,24 @@
 def solution(dartResult):
+    area = {"S":1, "D":2, "T":3}
     arr = []
     i = 0
     while i < len(dartResult):
         if dartResult[i].isdigit() and dartResult[i+1].isdigit():
-            arr.append(10)
+            arr.append("10")
             i += 2
         else:
             arr.append(dartResult[i])
             i += 1
-    return arr
+    stack = []
+    for dart in arr:
+        if dart.isdigit():
+            stack.append(int(dart))
+        elif dart in area:
+            stack[-1] = stack[-1]**area[dart]
+        elif dart == '*':
+            if len(stack) > 1:
+                stack[-2] = stack[-2]*2
+            stack[-1] = stack[-1]*2
+        else:
+            stack[-1] = -stack[-1]
+    return sum(stack)
