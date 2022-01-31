6be26ed8799316eb566039a1310752a26f146fd9
ghkdlxld <ohsg97@gmail.com>
2022-02-01 01:09:01 +0900

    소수 만들기

--- 소수_만들기.py
+++ 소수_만들기.py
@@ -1,2 +1,14 @@
+from itertools import combinations
+def is_sosu(num):
+    for i in range(2, num//2 + 1):
+        if num%i == 0:
+            return False
+    return True
+
 def solution(nums):
-    return answer
+    ans = 0
+    arr = list(combinations(nums, 3))
+    for numbers in arr:
+        if is_sosu(sum(numbers)):
+            ans += 1
+    return ans
