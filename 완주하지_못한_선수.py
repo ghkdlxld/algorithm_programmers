bf7f6cafa1e151fd7d44382d98cd2cd92a81b328
ghkdlxld <ohsg97@gmail.com>
2022-02-01 01:29:01 +0900

    완주하지 못한 선수

--- 완주하지_못한_선수.py
+++ 완주하지_못한_선수.py
@@ -1,14 +1,6 @@
 def solution(participant, completion):
-    answer = ''
-    temp = 0
-    dic = {}
-    for part in participant:
-        dic[hash(part)] = part
-        temp += int(hash(part))
-    for com in completion:
-        temp -= hash(com)
-    answer = dic[temp]
-
-    return answer
-
-print(solution(["marina", "josipa", "nikola", "vinko", "filipa"], ["josipa", "filipa", "marina", "nikola"]))
+    participant = sorted(participant)
+    completion = sorted(completion) + [False]
+    for i in range(len(participant)):
+        if participant[i] != completion[i]:
+            return participant[i]
