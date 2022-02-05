cc2f7d5031ea94e4e6e56194003438b3b0d3acdf
ghkdlxld <ohsg97@gmail.com>
2022-02-06 03:31:26 +0900

    신고 결과 받기

--- 신고_결과_받기.py
+++ 신고_결과_받기.py
@@ -1,3 +1,14 @@
 def solution(id_list, report, k):
-    answer = []
-    return answer
+    def mail(to):
+        for i in range(len(id_list)):
+            if link[to][i] == 1:
+                ans[i] += 1
+
+    ans = [0] * len(id_list)
+    link = [[0] * len(id_list) for _ in range(len(id_list))]
+    for ppl in report:
+        link[id_list.index(ppl.split()[1])][id_list.index(ppl.split()[0])] = 1  # to from
+    for i in range(len(id_list)):
+        if sum(link[i]) >= k:
+            mail(i)
+    return ans
