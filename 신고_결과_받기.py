69941c1da68c8eaaa13fddf0b3cfdf87c21aaa65
ghkdlxld <ohsg97@gmail.com>
2022-02-07 00:43:58 +0900

    2016년

--- 신고_결과_받기.py
+++ 신고_결과_받기.py
@@ -1,3 +1,20 @@
-def solution(id_list, report, k):
-    answer = []
-    return answer
+print("학점 계산기")
+subject_num = int(input("number of subject : "))
+
+arr = []
+for i in range(1, subject_num+1):
+    arr.append(int(input(f"adding your {i}_grade : ")))
+
+
+print("멘티여러분 고생많으셨습니다")
+avg = sum(arr)/subject_num
+print(f"Average : {avg}")
+
+if avg > 90:
+    grade = 'A'
+elif avg > 80:
+    grade = 'B'
+else:
+    grade = 'F'
+
+print(f"Your grade : {grade}")
